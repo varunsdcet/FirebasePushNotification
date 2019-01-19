9911776ab16de1c71506e2707de03dc29d340391
varunsdcet <33832368+varunsdcet@users.noreply.github.com>
2019-01-19 15:59:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,151 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,AsyncStorage,Alert} from 'react-native';
+import firebase from 'react-native-firebase';
+const messaging = firebase.messaging();
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+async getToken() {
+    alert('HI')
+  let fcmToken = await AsyncStorage.getItem('fcmToken', value);
+  if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        alert('ddd')
+          // user has a device token
+          await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+
+  }
+}
+
+  //2
+async requestPermission() {
+  try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      this.getToken();
+  } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+  }
+}
+
+
+async checkPermission() {
+  const enabled = await firebase.messaging().hasPermission();
+  if (enabled) {
+      this.getToken();
+  } else {
+      this.requestPermission();
+  }
+}
+
+  async componentDidMount() {
+
+    
+  const fcmToken = await firebase.messaging().getToken();
+if (fcmToken) {
+  alert('dd')
+    // user has a device token
+} else {
+    alert('ccc')// user doesn't have a device token yet
+}
+  this.checkPermission();
+  this.createNotificationListeners(); //add this line
+}
+
+////////////////////// Add these methods //////////////////////
+  
+  //Remove listeners allocated in createNotificationListeners()
+componentWillUnmount() {
+  this.notificationListener();
+  this.notificationOpenedListener();
+}
+
+async createNotificationListeners() {
+  /*
+  * Triggered when a particular notification has been received in foreground
+  * */
+  this.notificationListener = firebase.notifications().onNotification((notification) => {
+      const { title, body } = notification;
+      this.showAlert(title, body);
+  });
+
+  /*
+  * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+  * */
+  this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+      const { title, body } = notificationOpen.notification;
+      this.showAlert(title, body);
+  });
+
+  /*
+  * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+  * */
+  const notificationOpen = await firebase.notifications().getInitialNotification();
+  if (notificationOpen) {
+      const { title, body } = notificationOpen.notification;
+      this.showAlert(title, body);
+  }
+  /*
+  * Triggered for data only payload in foreground
+  * */
+  this.messageListener = firebase.messaging().onMessage((message) => {
+    //process data message
+    console.log(JSON.stringify(message));
+  });
+}
+
+showAlert(title, body) {
+  Alert.alert(
+    title, body,
+    [
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ],
+    { cancelable: false },
+  );
+}
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
